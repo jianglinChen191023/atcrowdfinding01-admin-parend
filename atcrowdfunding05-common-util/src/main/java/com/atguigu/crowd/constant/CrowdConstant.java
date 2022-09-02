e1628340aaed4d73545c0beb92c9548d7d2bcb50
jianglinChen191023 <1912623646@qq.com>
2022-09-02 15:27:41 +0800

    发起项目

--- atcrowdfunding05-common-util/src/main/java/com/atguigu/crowd/constant/CrowdConstant.java
+++ atcrowdfunding05-common-util/src/main/java/com/atguigu/crowd/constant/CrowdConstant.java
@@ -10,14 +10,22 @@ public class CrowdConstant {
     public static final String MESSAGE_ACCESS_DENIED = "抱歉! 您不能访问这个资源!";
     public static final String MESSAGE_CODE_NOT_EXISTS = "验证码已过期!请检查手机号码是否正确或重新发送!";
     public static final String MESSAGE_CODE_INVALID = "验证码不正确!";
+    public static final String MESSAGE_HEADER_PIC_UPLOAD_FAILED = "头图上传失败!";
+    public static final String MESSAGE_HEADER_PIC_EMPTY = "头图不可为空!";
+    public static final String MESSAGE_DETAIL_PIC_EMPTY = "详情图片不可为空!";
+    public static final String MESSAGE_DETAIL_PIC_UPLOAD_FAILED = "详情图片上传失败!";
+    public static final String MESSAGE_TEMPLE_PROJECT_MISSING = "临时存储的 projectVO 对象丢失!";
+    public static final String MESSAGE_DETAIL_CONFIRM = "确认信息失败! 请先填写项目及发起人信息";
 
     public static final String ATTR_NAME_LOGIN_ADMIN = "loginAdmin";
     public static final String ATTR_NAME_EXCEPTION = "exception";
     public static final String ATTR_NAME_PAGE_INFO = "pageInfo";
-    public static final String REDIS_CODE_PREFIX = "REDIS_CODE_PREFIX_";
     public static final String ATTR_NAME_MESSAGE = "message";
     public static final String ATTR_NAME_LOGIN_MEMBER = "loginMember";
+    public static final String ATTR_NAME_TEMPLE_PROJECT = "tempProject";
+
+    public static final String REDIS_CODE_PREFIX = "REDIS_CODE_PREFIX_";
 
     public static final String ZUUL_PATH_KEY = "zuulPath";
-    public static final String ZUUL_PATH_VALUE = "http://localhost";
+    public static final String ZUUL_PATH_VALUE = "http://s1.com";
 }
